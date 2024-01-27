460d844d42b9cf868f40f7e715985d20501a9044
pruthvesh <code.pruthvesh@gmail.com>
2024-01-27 12:00:28 +0530

    changed project file structure

---
+++ client/src/pages/api/createTable.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import createTables from "../../../server/db/createTable";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    await createTables();
+
+    res.status(200).json({ message: "tables created" });
+}
---
+++ client/src/pages/api/login.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { inputValidate } from "@/lib/inputValidation";
+import checkUser from "../../../server/db/checkUser";
+import jwt from 'jsonwebtoken'
+import { log } from "console";
+import { SECRET } from "@/config";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const loginInput: { email: string, password: string } = JSON.parse(req.body);
+    const parsedInput = inputValidate.safeParse(loginInput);
+
+    if (!parsedInput.success) {
+        res.status(401).json({ message: "wrong input" });
+        return;
+    }
+
+    const userId = await checkUser(loginInput);
+    if (userId) {
+        const token = jwt.sign({ userId, ...loginInput }, SECRET, { expiresIn: '3hr' });
+        res.status(200).json({ token, message: "logged in successfull" });
+    }
+    else {
+        res.status(403).json({ message: "User does not exist" })
+    }
+
+}
